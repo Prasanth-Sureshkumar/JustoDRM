fe8dcb6c3b64a83238dca88b5d5c2cd03efe10fe
Prasanth-Sureshkumar <prasanths2990@gmail.com>
2025-10-07 16:08:49 +0530

    final cdoe

--- src/constants/apiUrls.js
+++ src/constants/apiUrls.js
@@ -1,6 +1,6 @@
-export const API_BASE_URL = '/api';
+export const APP_BASE_URL = '/api';
 
-export const LOGIN_URL = `${API_BASE_URL}/login`;
-export const GET_ALL_BOOKS = `${API_BASE_URL}/secure-asset`;
-export const GET_ALL_AUDIOS = `${API_BASE_URL}/secure-audio`;
-export const AQUIRE_LICENSE = `${API_BASE_URL}/license`;
+export const LOGIN_URL = `${APP_BASE_URL}/login`;
+export const GET_ALL_BOOKS = `${APP_BASE_URL}/secure-asset`;
+export const GET_ALL_AUDIOS = `${APP_BASE_URL}/secure-audio`;
+export const AQUIRE_LICENSE = `${APP_BASE_URL}/license`;
